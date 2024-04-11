31a1e0285acb665f84cdcaa157312232c3881b93
filiperiksson4 <filip999@live.se>
2024-04-11 17:33:14 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,40 @@ void main() async {
 
   await Hive.initFlutter();
 
-  final storage = await Hive.openBox<dynamic>('drawing');
-  final drawingRepository = DrawingRepository(storage);
+  //final storage = await Hive.openBox<dynamic>('drawing');
+  //final drawingRepository = DrawingRepository(storage);
+
+  Future<Box<dynamic>> waitedOpenBox() async {
+    await Future.delayed(const Duration(seconds: 2));
+    return Hive.openBox<dynamic>('drawing');
+  }
+
+  final appBuilder = FutureBuilder(
+    future: waitedOpenBox(),
+    builder: (context, AsyncSnapshot<Box<dynamic>> snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        // While waiting for data, display a loading indicator
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      } else if (snapshot.hasError) {
+        // If an error occurs, display an error message
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Home'),
+          ),
+          body: Center(
+            child: Text('Error: ${snapshot.error}'),
+          ),
+        );
+      } else {
+        return App(DrawingRepository(snapshot.data!));
+      }
+    },
+  );
 
   runApp(
-    App(drawingRepository),
+    //App(drawingRepository),
+    appBuilder,
   );
 }
