e8526c7d5fdf8f9f25a11e0b301d579c619b47fe
Einar Johansson <ejrobb09@gmail.com>
2024-04-11 16:59:39 +0200

    Co-authored-by: cecahn <cecahn@users.noreply.github.com>

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -12,9 +12,55 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return RepositoryProvider.value(
       value: _drawingRepository,
-      child: const MaterialApp(
-        home: DrawingBoardPage(),
+      child: MaterialApp(
+        home: _HomePage(_drawingRepository),
       ),
     );
+  }
+}
+
+class _HomePage extends StatefulWidget {
+  const _HomePage(this._drawingRepository, {super.key});
+  final DrawingRepository _drawingRepository;
+
+  @override
+  __HomePageState createState() => __HomePageState();
+}
+
+class __HomePageState extends State<_HomePage> {
+  bool _isLoading =
+      true; // Initially set to true to show the circular progress indicator
+
+  @override
+  void initState() {
+    super.initState();
+    // Simulate fetching data
+    _fetchData();
+  }
+
+  void _fetchData() {
+    // Simulate network request delay
+    Future.delayed(const Duration(seconds: 2), () {
+      setState(() {
+        _isLoading = false;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _isLoading
+        ? const Scaffold(
+            body: Center(
+              child:
+                  CircularProgressIndicator(), // Display circular progress indicator
+            ),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              title: const Text('Drawing Board'),
+            ),
+            body: const DrawingBoardPage(),
+          );
   }
 }
